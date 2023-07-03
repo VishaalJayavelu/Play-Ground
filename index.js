380801ca3a55311df3f9527112ce1128f197ea1d
Vishaal Jayavelu <vishaal.j.trs@gmail.com>
2023-07-03 17:16:55 +0530

    to-do list

--- index.js
+++ index.js
@@ -1,30 +1,58 @@
-let todoTask = [];
+window.onload = function() {
+     const addtask=document.querySelector("#add-task");
+     const tasktext=document.querySelector("#task-text");
+     const tasklist=document.querySelector("#tasks");
+     addtask.addEventListener("submit",(e)=>{
+          e.preventDefault();
+          
+          const task=tasktext.value;
+          if(!task){
+               alert("Please fill the task");
+               return;
+          } 
 
-function add(text) {
-  const todo = {
-    text,
-    checked: false,
-    id: Date.now(),
-  };
+          const text=document.createElement("input");
+          text.classList.add("text");
+          text.type="text";
+          text.value=task;
+          text.setAttribute("readonly", "readonly");
 
-  todoTask.push(todo);
-  console.log(todoTask);
+          const deletebnt=document.createElement("button");
+          deletebnt.classList.add("delete");
+          deletebnt.innerText="Delete";
+
+          const editbnt=document.createElement("button");
+          editbnt.classList.add("edit");
+          editbnt.innerText="Edit";
+
+          const action=document.createElement("span");
+          action.classList.add("actions");
+          action.appendChild(editbnt);
+          action.appendChild(deletebnt);
+
+          const taskwarp=document.createElement("div");
+          taskwarp.classList.add("taskwarp");
+          taskwarp.appendChild(text);
+          taskwarp.appendChild(action);
+          tasklist.appendChild(taskwarp);
+          tasklist.appendChild(document.createElement('br'));
+          
+          tasktext.value = "";
+    
+          deletebnt.addEventListener("click",(e)=>{
+               tasklist.removeChild(taskwarp);
+          });
+ 
+          editbnt.addEventListener("click",(e)=>{
+               if (editbnt.innerText.toLowerCase()=='edit') {
+                    editbnt.innerText="Save";
+                    text.removeAttribute("readonly");
+                    text.focus();
+               } else {
+                    editbnt.innerText="Edit";
+                    text.setAttribute("readonly", "readonly");
+               }
+           });
+
+     });
 }
-
-const form = document.querySelector('.Add-Task');
-
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  const input = document.querySelector('.task');
-  const text = input.value.trim();
-  if (text !== '') {
-    add(text);
-    input.value = '';
-    input.focus();
-  }
-});
-
-
-
-
-
