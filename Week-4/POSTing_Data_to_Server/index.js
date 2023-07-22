c4b60993f8269bfc0f27b263b2a72611ebf318db
Vishaal Jayavelu <vishaal.j.trs@gmail.com>
2023-07-22 12:51:35 +0530

    Changes

---
+++ Week-4/POSTing_Data_to_Server/index.js
@@ -0,0 +1,49 @@
+const http = require('http'); 
+
+const PORT = 5050;
+
+const list=[
+     {
+          id:0,
+          name:"testing done",
+     },
+     {
+          id:1,
+          name:"testing",
+     },
+     {
+          id:2,
+          name:"tested",
+     },
+     {
+          id:3,
+          name:"need to test",
+     },
+];
+
+const server=http.createServer();
+server.on('request', (req, res)=> {
+     const item = req.url.split('/');
+     if(item[1]==='list'){
+          res.writeHead(200, {'Content-type': 'application/json'}); 
+          if(item.length===3){
+               const index = Number(item[2]);
+               res.write(JSON.stringify(list[index]));
+          }else  {
+               res.write(JSON.stringify(list));           
+          }
+          res.end();
+     }else if(item[1]==='html'){
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'text/html');
+          res.write('<html><body><ul><li>html</li></ul></body></html>');
+          res.end();
+     }else{
+          res.statusCode = 404;
+          res.end('Not Found');
+     }
+});
+
+server.listen(PORT, ()=>{
+     console.log(`Listening on port ${PORT}`);
+});  
